0fa47e837dff507c6da1b60f593a9668550c2dfc
Juan Pablo Martínez <jpablo.mguzman@gmail.com>
2024-12-17 10:10:32 -0500

    feat: create event controller & initial code for transforms

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,9 +4,8 @@ using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
-    private PlayerInputs _playerInputs;
     private Rigidbody2D _rb;
-    private Vector2 _moveInput;
+    public Vector2 _moveInput;
 
     [Header("Movement parameters")]
     [SerializeField] private float moveSpeed = 5f;
@@ -14,21 +13,20 @@ public class MovementController : MonoBehaviour
     [Header("Dash parameters")]
     [SerializeField] private float dashSpeed = 20f;
     [SerializeField] private float dashDuration = 0.2f;
-    [SerializeField] private float dashCoolDown = 2f;
-    private float dashTimer;
-    private bool isDashing;
+    public float dashCoolDown = 2f;
+    public float dashTimer;
+    public bool isDashing;
     private float dashEndTime;
 
-    private void Awake() {
-        _playerInputs = new();
-    }
+    
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update() {
+    private void FixedUpdate()
+    {
         dashTimer += Time.deltaTime;
 
         // If we are currently dashing, check if the dash duration has ended
@@ -37,10 +35,7 @@ public class MovementController : MonoBehaviour
             // Dash is over
             isDashing = false;
         }
-    }
 
-    private void FixedUpdate()
-    {
         // If currently dashing, do not apply normal movement
         if (!isDashing)
         {
@@ -48,65 +43,19 @@ public class MovementController : MonoBehaviour
         }
     }
 
-    #region InputsEvents
-
-    private void OnEnable() 
-    {
-        _playerInputs.Player.Enable();
-
-        _playerInputs.Player.Movement.performed += OnMove;
-        _playerInputs.Player.Movement.canceled += CancelMove;
-
-        _playerInputs.Player.Dash.performed += OnDash;
-    }
-
-    private void OnDisable() 
-    {
-        _playerInputs.Player.Disable();
-
-        _playerInputs.Player.Movement.performed -= OnMove;
-        _playerInputs.Player.Movement.canceled -= CancelMove;
-
-        _playerInputs.Player.Dash.performed -= OnDash;
-    }
-
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        _moveInput = context.ReadValue<Vector2>();
-    }
-
-    private void CancelMove(InputAction.CallbackContext context)
-    {
-        _moveInput = Vector2.zero;
-    }
-
-    private void OnDash(InputAction.CallbackContext context)
-    {
-        if (context.performed && dashTimer >= dashCoolDown)
-        {
-            // Perform dash
-            Dash();
-        }
-    }
-
-    #endregion
-
     private void HandleMovement()
     {
         //apply to the RigidBody2D velocity in the direction of the input at velocity of moveSpeed
         _rb.velocity =_moveInput * moveSpeed;
     }
-    private void Dash()
+    public void Dash()
     {
         // Set dashing state
         isDashing = true;
-        dashEndTime = Time.time + dashDuration;
+        dashEndTime = Time.time + dashDuration; //start dash timer
         dashTimer = 0f;
 
-        // Apply a high velocity directly (instead of AddForce)
-        // This ensures immediate high-speed movement without waiting for physics
+        // Apply a high velocity in movement direction
         _rb.velocity = _moveInput.normalized * dashSpeed;
-
-        Debug.Log("DASH");
     }
 }
