37858ba590908244b353e4d89c048bba91301257
Juan Pablo Martínez <jpablo.mguzman@gmail.com>
2024-12-23 11:01:14 -0500

    feat: create win controller & logic for pause menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Controllers")]
+    [SerializeField] private GameObject player;
+
+
+    [Header("UI elements")]
     [SerializeField] private Text pointsText;
 
     [SerializeField] private Image dashTimer;
-    private float dashCD;
 
-    [SerializeField] private GameObject player;
+
+    [Header("Win Conditions")]
+    [SerializeField] private GameObject winZone;
+    [SerializeField] private int pointsToWin;
 
     private MovementController movementController;
     private EatController eatController;
@@ -20,8 +29,6 @@ public class GameController : MonoBehaviour
     {
         eatController = player.GetComponent<EatController>();
         movementController = player.GetComponent<MovementController>();
-
-        dashCD = movementController.dashCoolDown;
     }
 
     // Update is called once per frame
@@ -29,14 +36,41 @@ public class GameController : MonoBehaviour
     {
         pointsText.text = "x " + eatController.points;
         UpdateDashFill();
-
+        ActivateWinZone(eatController.points);
     }
 
     private void UpdateDashFill()
     {
-
         // Here we map the timer to a 0-1 range for the fill amount
         float fillValue = movementController.dashTimer / movementController.dashCoolDown;
         dashTimer.fillAmount = Mathf.Clamp01(fillValue);
+    }
+
+    private void ActivateWinZone(int point)
+    {
+        if (point >= pointsToWin)
+        {
+            winZone.SetActive(true);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+     public void LoadScene(String sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
